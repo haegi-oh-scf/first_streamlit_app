7892d633fa5f8fe8614cac8b495b07b31ab3ad73
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-10 15:03:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ streamlit.dataframe(fruits_to_show)
 #create function (repeatable block of code)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
-    fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
 #New section to display fruitvice api response

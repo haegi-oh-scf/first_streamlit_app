9a8808109558dbb8ad5cc17488d2f1d849aaa0be
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-10 11:24:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - outputs data as table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
+
 
